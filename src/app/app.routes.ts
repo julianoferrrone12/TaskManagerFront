95fe10a195d19a357ef6398a275eca0d34eaa26c
julianoferrrone12 <julianofcaetano@gmail.com>
2024-07-31 11:43:34 -0300

    feat: sistema de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TaskManagerComponent } from './taskManager/taskManager.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/task-list', pathMatch: 'full' },
+    { path: 'task-list', component: TaskManagerComponent },
+];
